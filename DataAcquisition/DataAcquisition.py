2c9d654795603006b2e1bb932ef2dd99b6b33443
AlessandroRedi <alessandroredi1@gmail.com>
2023-11-27 14:01:32 +0100

    SNMP calls added

---
+++ DataAcquisition/DataAcquisition.py
@@ -0,0 +1,41 @@
+import os
+from DataAcquisition.config import *
+
+
+class DataAcquisition:
+    def __init__(self):
+        pass
+
+    def acquier(self):
+        os.system(
+            f"DataAcquisitionwalk -v2c {COMMUNITY} {IP_WLC} {OID_MAC} > SNMP_Files/{FILE_MAC}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_USERNAME} > SNMP_Files/{FILE_USERNAME}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_RSSI} > SNMP_Files/{FILE_RSSI}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_BYTES_TX} > SNMP_Files/{FILE_BYTES_TX}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_BYTES_RX} > SNMP_Files/{FILE_BYTES_RX}"
+        )
+
+    def acquier_AP(self):
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_AP_MAC} > SNMP_Files/{FILE_AP_MAC}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_AP_WEB} > SNMP_Files/{FILE_AP_WEB}"
+        )
+
+        os.system(
+            f"snmpwalk -v2c {COMMUNITY} {IP_WLC} {OID_AP_NAME} > SNMP_Files/{FILE_AP_NAME}"
+        )
---
+++ DataAcquisition/config.py
@@ -0,0 +1,25 @@
+# Configuration file
+
+# snmpwalk command information
+COMMUNITY = "-c abcdefghi"
+IP_WLC = "130.192.xx.yy"
+OID_MAC = "1.3.6.1.4.1.14179.2.1.4.1.1"
+OID_USERNAME = "1.3.6.1.4.1.14179.2.1.4.1.3"
+OID_RSSI = "1.3.6.1.4.1.14179.2.1.6.1.1"
+OID_SNR = "1.3.6.1.4.1.14179.2.1.6.1.26"
+OID_BYTES_TX = "1.3.6.1.4.1.14179.2.1.6.1.2"
+OID_BYTES_RX = "1.3.6.1.4.1.14179.2.1.6.1.3"
+OID_AP_MAC = "1.3.6.1.4.1.14179.2.1.4.1.4"
+OID_AP_WEB = "1.3.6.1.4.1.9.9.513.1.2.3.1.1"
+OID_AP_NAME = "1.3.6.1.4.1.14179.1.1.1.1.5"
+
+# Files names
+FILE_MAC = "mac.txt"
+FILE_USERNAME = "username.txt"
+FILE_RSSI = "rssi.txt"
+FILE_SNR = "snr.txt"
+FILE_BYTES_TX = "bytes_tx.txt"
+FILE_BYTES_RX = "bytes_rx.txt"
+FILE_AP_MAC = "ap_mac.txt"
+FILE_AP_WEB = "ap_web.txt"
+FILE_AP_NAME = "ap_name.txt"
