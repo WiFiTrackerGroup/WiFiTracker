9080aa8d1cdb459527e1713cab6efbe99dd770c8
Giovanni Dettori <giovi.detto2000@gmail.com>
2023-11-27 14:21:53 +0100

    parser implemented

--- data_aggregation_class/DataAggregation.py
+++ data_aggregation_class/DataAggregation.py
@@ -1,6 +1,21 @@
 import pandas as pd
 from config import *
 import re
+import parse
+
+def _parse_line(line):
+        """
+        
+        """
+        rx_dict = {
+            'mac': re.compile(r'iso.3.6.1.4.1.14179.2.1.4.1.3.(?P<mac>.*) ='),
+        }
+        for key, rx in rx_dict.items():
+            match = rx.search(line)
+            if match:
+                return key, match
+        # if there are no matches
+        return None, None
 
 class DataAggregation:
 
@@ -14,25 +29,20 @@ class DataAggregation:
     def open_username(self):
         data = []
         with open(FILE_USERNAME, 'r') as file_object:
-            line = file_object.readline()
-            while line:
-                key, match = _parse_line(line)
-                if key == mac:
-                    print("ciao")
+            for line in file_object:
+                mac_res = parse.search('iso.3.6.1.4.1.14179.2.1.4.1.3.{} =', line).fixed
+                dom_res = parse.search('@{}"', line).fixed
+                row = {
+                    'mac': mac_res[0],
+                    'domain': dom_res[0]
+                }
+                data.append(row)
+        
+        data = pd.DataFrame(data)
+        print(data)
+                
 
-    def _parse_line(line):
-    """
-    ciao
-    """
-    rx_dict = {
-        'mac': re.compile(r'iso.3.6.1.4.1.14179.2.1.4.1.3.(?P<mac>.*) '),
-    }
-    for key, rx in rx_dict.items():
-        match = rx.search(line)
-        if match:
-            return key, match
-    # if there are no matches
-    return None, None
+    
                 
 
 elem = DataAggregation()
