2d615b644cb0397e771228aff5430fbd4f2f6f13
Giovanni Dettori <giovi.detto2000@gmail.com>
2024-03-06 19:00:18 +0100

    Update: config file of acquisition changed to work on local mongodb

--- Service/sub/config.py
+++ Service/sub/config.py
@@ -16,6 +16,14 @@ TRACKNAME = "tracking"
 URL_DB = (
     f"mongodb+srv://{USER_MONGODB}:{PSW_MONGODB}@wifitracker2024.fqyv89v.mongodb.net/"
 )
+# ----------------------------------------------------------------------------------------
+# Config for local mongoDB
+# PSW_MONGODB = "hcgFRD3G!f"
+# USER_MONGODB = "wifitracker"
+# HOST_MONGODB = "127.0.0.1"
+# PORT_MONGODB = "27017"
+# URL_DB = f"mongodb://{USER_MONGODB}:{PSW_MONGODB}@{HOST_MONGODB}:{PORT_MONGODB}/{DBNAME}"
+# ----------------------------------------------------------------------------------------
 
 FILE_ERRORS = "Service/sub/log/mongoDB_log.txt"
 FILE_HISTORY = "Service/history/"
