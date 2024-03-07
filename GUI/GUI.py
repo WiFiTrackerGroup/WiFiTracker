20f3f7e24f506983c58c85d764862756029e5055
umberto150301 <umberto150301@gmail.com>
2024-03-07 11:43:33 +0100

    Started GUI Added directory with room images No communicatio with Mongo implemented Yet

---
+++ GUI/GUI.py
@@ -0,0 +1,101 @@
+from pymongo import MongoClient
+import streamlit as st
+import folium
+from folium.plugins import HeatMap
+from datetime import datetime
+import config
+from PIL import Image
+import io
+import os
+
+PATHS = config.image_path
+
+def selection(rooms):
+
+    # Possible room choice and selection
+    rooms.insert(0, "--select--")
+    choice = st.selectbox("Select a room", rooms)
+
+    # Time selection
+    current = st.checkbox("Use current date and hour")
+    if current:
+        # To use current date and time 
+        date = datetime.now().date()
+        time = datetime.now().time()
+    else:
+        # To select date and time 
+        date = st.date_input("Select date")
+        time = st.time_input("Select hour")
+
+    return choice, date, time
+
+def visualization(choice, date, time):
+
+    # Format
+    date = date.strftime("%d %B %Y, %A")
+    time = time.strftime("%I:%M")
+
+    # Visualization
+    st.write(f"<strong>Selected Room:</strong> {choice}", unsafe_allow_html=True)
+    st.write(f"<strong>Selected date and hour:</strong> {date} {time}", unsafe_allow_html=True)
+
+def check(date, time):
+
+    # Current
+    date_now = datetime.now().date()
+    time_now = datetime.now().time()
+
+    # Check that the date is 
+    if date_now<date:
+        return False
+    elif date_now == date:
+        if time_now < time:
+            return False
+        else:
+            return True
+    else:
+        return True
+        
+def visualizeMap(choice):
+
+    # Check if the choice has been made
+    if not choice in PATHS:
+        return
+    
+    # Select path
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(path, PATHS[choice])
+
+    # Convert image to PNG and visualize
+    image_bmp = Image.open(path)
+    png_byte_array = io.BytesIO()
+    image_bmp.save(png_byte_array, format='PNG')
+    st.image(png_byte_array, use_column_width=True)
+
+
+
+
+def main():
+
+    # Sreate the application and give a title
+    st.title("Room occupancy visualization")
+
+    # Select available rooms from constants file
+    rooms = list(PATHS.keys())
+
+    # take the choice of the user and visualize it
+    choice, date, time = selection(rooms)
+    visualization(choice, date, time)
+
+    # Wrong date and time warning
+    if not check(date, time):
+        st.write('<span style="color:red">Data not available!</span>', unsafe_allow_html=True)
+        st.write('<span style="color:red">Please inserta a valid date!</span>', unsafe_allow_html=True)
+
+    visualizeMap(choice)
+
+
+    
+
+if __name__ == "__main__":
+    main()
---
+++ GUI/config.py
@@ -0,0 +1,5 @@
+image_path = {"Even": "Rooms\Room_Even.bmp", 
+              "Odd": "Rooms\Room_Odd.bmp", 
+              "P":"Rooms\Room_P.bmp", 
+              "R Ground Floor": "Rooms\Room_R_G.bmp", 
+              "R First Floor": "Rooms\Room_R_1.bmp"}
