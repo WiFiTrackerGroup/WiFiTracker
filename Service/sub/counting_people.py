8187dfd1ef846f259e3d67ef95a045e4bdd0ef47
federicovolponi <federicovolponi99@gmail.com>
2024-04-02 15:11:18 +0200

    solved minor issue and removed redundant function

--- Service/sub/counting_people.py
+++ Service/sub/counting_people.py
@@ -69,7 +69,6 @@ class Counting_P:
             "ch_util_5_mean": [],
             "noise_2_4_mean": [],
             "noise_5_mean": [],
-            "n_people": [],
         }
         rooms = dataRoom["Room"].unique()
         for room in rooms:
@@ -111,73 +110,7 @@ class Counting_P:
         return df_features
 
     def random_forest_regr(self, dataRoom: pd.DataFrame):
-        timestamp = dataRoom["Timestamp"].iloc[0]
-        # EXtract features
-        df_features = self.feature_extraction(dataRoom)
-        X = df_features.loc[:, df_features.columns != "room"].to_numpy()
-        y = self.rndForest_regr.predict(X)
-        df_features["N_people"] = y
-        dataRoom = dataRoom.join(df_features[["room", "N_people"]], on="Room")
-        dataRoom = dataRoom.reset_index()
-        dataRoom["Timestamp"] = timestamp
-        return dataRoom
-
-    def feature_extraction(self, dataRoom: pd.DataFrame):
-        feature_dict = {
-            "room": [],
-            "n_devices": [],
-            "n_users": [],
-            "snr_mean": [],
-            "snr_std": [],
-            "rssi_mean": [],
-            "rssi_std": [],
-            "ch_util_2_4_mean": [],
-            "ch_util_5_mean": [],
-            "noise_2_4_mean": [],
-            "noise_5_mean": [],
-            "n_people": [],
-        }
-        rooms = dataRoom["Room"].unique()
-        for room in rooms:
-            df_room = dataRoom[dataRoom["Room"] == room]
-            feature_dict["room"].append(room)
-
-            # Feature extrapolation
-            df_dist_ap = df_room.drop_duplicates(subset="APnum")
-            n_devices = (
-                df_dist_ap["n_clients_2_4"].sum() + df_dist_ap["n_clients_5"].sum()
-            )
-            feature_dict["n_devices"].append(n_devices)
-
-            df_dist_users = df_room.drop_duplicates(subset="user_masked")
-            n_users = len(df_dist_users[df_dist_users["snr"] > 20])
-            feature_dict["n_users"].append(n_users)
-
-            snr_mean = df_room["snr"].mean()
-            feature_dict["snr_mean"].append(snr_mean)
-            snr_std = df_room["snr"].std()
-            feature_dict["snr_std"].append(snr_std)
-
-            rssi_mean = df_room["rssi"].mean()
-            feature_dict["rssi_mean"].append(rssi_mean)
-            rssi_std = df_room["rssi"].std()
-            feature_dict["rssi_std"].append(rssi_std)
-
-            ch_util_2_4_mean = df_dist_ap["ch_utilization_2_4"].mean()
-            feature_dict["ch_util_2_4_mean"].append(ch_util_2_4_mean)
-            ch_util_5_mean = df_dist_ap["ch_utilization_5"].mean()
-            feature_dict["ch_util_5_mean"].append(ch_util_5_mean)
-
-            noise_2_4_mean = df_dist_ap["noise_2_4"].mean()
-            feature_dict["noise_2_4_mean"].append(noise_2_4_mean)
-            noise_5_mean = df_dist_ap["noise_5"].mean()
-            feature_dict["noise_5_mean"].append(noise_5_mean)
-
-        df_features = pd.DataFrame.from_dict(feature_dict)
-        return df_features
-
-    def random_forest_regr(self, dataRoom: pd.DataFrame):
-        timestamp = dataRoom["Timestamp"].iloc[0]
+        timestamp = dataRoom["Timestamp_x"].iloc[0]
         # EXtract features
         df_features = self.feature_extraction(dataRoom)
         X = df_features.loc[:, df_features.columns != "room"].to_numpy()
