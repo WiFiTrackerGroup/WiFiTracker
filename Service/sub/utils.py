0a0cd797a7d611369f92aa20ede68a12eba96bd9
Giovanni Dettori <giovi.detto2000@gmail.com>
2024-04-07 17:40:28 +0200

    correction: function roomDivision in utils

--- Service/sub/utils.py
+++ Service/sub/utils.py
@@ -13,9 +13,22 @@ def room_division(dataRoom):
     dataRoom = dataRoom[dataRoom["name_ap"].notnull()]
     dataRoom = dataRoom[dataRoom["name_ap"].str.contains(Ap)]
     rooms = pd.DataFrame()
-    rooms[["AP", "Room", "APnum", "NaN"]] = dataRoom["name_ap"].str.split(
-        "-", expand=True
-    )
+    max_l = max([len(sublist.split("-")) for sublist in dataRoom["name_ap"]])
+    if max_l == 2:
+        rooms[["AP", "Room"]] = dataRoom["name_ap"].str.split("-", expand=True)
+    elif max_l == 3:
+        rooms[["AP", "Room", "APnum"]] = dataRoom["name_ap"].str.split("-", expand=True)
+    elif max_l == 4:
+        rooms[["AP", "Room", "APnum", "NaN"]] = dataRoom["name_ap"].str.split(
+            "-", expand=True
+        )
+    elif max_l == 5:
+        rooms[["AP", "Room", "APnum", "NaN", "other"]] = dataRoom["name_ap"].str.split(
+            "-", expand=True
+        )
+    else:
+        raise Exception("Error in dataRoom['name_ap'].str.split('-', expand=True)")
+
     dataRoom = pd.concat([dataRoom, rooms], axis=1)
     dataRoom = dataRoom.drop(["AP", "NaN"], axis=1)
     return dataRoom
